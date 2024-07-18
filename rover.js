2a204945c5860e121aadb48b972f5296649e782e
aish.dande9 <aish.dande@gmail.com>
2024-07-18 17:59:57 -0500

    changes made

--- rover.js
+++ rover.js
@@ -1,5 +1,5 @@
-const Message = require("./message.js");
-const Command = require("./command.js");
+// const Message = require("./message.js");
+// const Command = require("./command.js");
 class Rover {
   // Write code here!
   constructor(position) {
@@ -12,20 +12,21 @@ class Rover {
     const response = { message: message.name };
     const result = [];
     for (let i = 0; i < message.commands.length; i++) {
-      if (message.commands[i].commandType === "STATUS_CHECK") {
+      const command = message.commands[i];
+      if (command.commandType === "STATUS_CHECK") {
         const roverStatus = {
           position: this.position,
           mode: this.mode,
           generatorWatts: this.generatorWatts,
         };
         result.push({ completed: true, roverStatus });
-      } else if (message.commands[i].commandType === "MODE_CHANGE") {
+      } else if (command.commandType === "MODE_CHANGE") {
         result.push({ completed: true });
-        this.mode = message.commands[i].value;
+        this.mode = command.value;
         //console.log(this.mode);
-      } else if (message.commands[i].commandType === "MOVE") {
+      } else if (command.commandType === "MOVE") {
         if (this.mode === "NORMAL") {
-          this.position = message.commands[i].value;
+          this.position = command.value;
           result.push({ completed: true });
         } else {
           result.push({ completed: false });
@@ -39,17 +40,17 @@ class Rover {
   }
 }
 
-let commands = [
-  new Command("STATUS_CHECK"),
-  new Command("MOVE", 4321),
-  new Command("STATUS_CHECK"),
-  new Command("MODE_CHANGE", "LOW_POWER"),
-  new Command("MOVE", 3579),
-  new Command("STATUS_CHECK"),
-];
-let message = new Message("Test message with two commands", commands);
-let rover = new Rover(98382); // Passes 98382 as the rover's position.
-let response = rover.receiveMessage(message);
+// let commands = [
+//   new Command("STATUS_CHECK"),
+//   new Command("MOVE", 4321),
+//   new Command("STATUS_CHECK"),
+//   new Command("MODE_CHANGE", "LOW_POWER"),
+//   new Command("MOVE", 3579),
+//   new Command("STATUS_CHECK"),
+// ];
+// let message = new Message("Test message with two commands", commands);
+// let rover = new Rover(98382); // Passes 98382 as the rover's position.
+// let response = rover.receiveMessage(message);
 
-console.log(response);
+// console.log(response);
 module.exports = Rover;
